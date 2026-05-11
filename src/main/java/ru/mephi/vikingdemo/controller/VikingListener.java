3ee31c3c4484b94df778fd0d751b2af301e414e9
KSAbramova <263575666+KSAbramova@users.noreply.github.com>
2026-05-11 16:43:30 +0300

    Update VikingListener.java

--- src/main/java/ru/mephi/vikingdemo/controller/VikingListener.java
+++ src/main/java/ru/mephi/vikingdemo/controller/VikingListener.java
@@ -1,18 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ru.mephi.vikingdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.mephi.vikingdemo.gui.VikingDesktopFrame;
+import ru.mephi.vikingdemo.model.Viking;
 import ru.mephi.vikingdemo.service.VikingService;
 
-/**
- *
- * @author test2023
- */
 @Component
 public class VikingListener {
     private VikingService service;
@@ -29,5 +22,23 @@ public class VikingListener {
 
     void testAdd() {
         gui.addNewViking(service.createRandomViking());
+    }
+
+    public void notifyVikingAdded(Viking viking) {
+        if (gui != null) {
+            gui.addNewViking(viking);
+        }
+    }
+
+    public void notifyVikingDeleted(String name) {
+        if (gui != null) {
+            gui.removeVikingByName(name);
+        }
+    }
+
+    public void notifyVikingUpdated(Viking viking) {
+        if (gui != null) {
+            gui.updateVikingByName(viking);
+        }
     }
 }
